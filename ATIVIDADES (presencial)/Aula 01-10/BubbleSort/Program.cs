2aa0e7a48cc29edb0695c7dfb22b2f115ef03eb5
KauannyRibeiro <kaufree2@gmail.com>
2025-10-01 21:59:49 -0300

    Atualizado

---
+++ ATIVIDADES (presencial)/Aula 01-10/BubbleSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using BubbleSort;
+
+Console.WriteLine("Array não ordenado: ");
+Console.WriteLine(" ");
+
+int[] arrNumbers =
+    new int[] { 99, 50, -24, 0, 1 };
+
+foreach (var number in arrNumbers)
+{
+    Console.Write($"[{number}] ");
+}
+
+var arrOrdered = BubbleSortOrder.Sort<int>(
+        arrNumbers
+        );
+
+Console.WriteLine(" ");
+Console.WriteLine("********");
+Console.WriteLine(" ");
+Console.WriteLine("Array Ordenado: ");
+Console.WriteLine(" ");
+
+foreach (var number in arrOrdered)
+{
+    Console.Write($"[{number}] ");
+}
---
+++ ATIVIDADES (presencial)/Aula 01-10/BubbleSortVetorString/Program.cs
@@ -0,0 +1,20 @@
+﻿using BubbleSortVetorString;
+
+Console.WriteLine("Array de string não ordenado: ");
+Console.WriteLine(" ");
+
+string[] arrNumbers =
+    new string[] {"Ana", "Luana", "Rafael", "Joao", "Eduardo", "Maria", "Beatriz", "Carlos"};
+
+foreach (var number in arrNumbers)
+    Console.Write($"[{number}] ");
+
+var arrOrdered = BubbleSortOrder.Sort<string>(arrNumbers);
+
+Console.WriteLine(" ");
+Console.WriteLine("*****************************");
+Console.WriteLine("Array de string ordenado: ");
+Console.WriteLine(" ");
+
+foreach (var number in arrOrdered)
+    Console.Write($"[{number}] ");
