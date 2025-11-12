b64445b0e41c7396d7cc87ee4bd83f1b0071a025
KauannyRibeiro <kaufree2@gmail.com>
2025-11-12 20:09:13 -0300

    Atualizado

---
+++ ATIVIDADES (presencial)/Aula 15-10/SelectionSortt/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] integerValues = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+SelectionSortt.SelectionSort.Sort(integerValues);
+
+Console.WriteLine("Vetor de inteiros");
+Console.WriteLine(string.Join(",", integerValues));
+
+Console.WriteLine("********************************");
+
+Console.WriteLine("Vetor de nomes");
+string[] stringValues = { "Bozonaro", "LombrigaBR", "Virginia", "Juliana", "Mc Lan", "Ze Felipe", "Luan Pereira" };
+SelectionSortt.SelectionSort.Sort(stringValues);
+Console.WriteLine(string.Join(",", stringValues));
---
+++ ATIVIDADES (presencial)/Aula 22-10/QuickSortt/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] integerValues = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+QuickSortt.QuickSort.Sort(integerValues);
+
+Console.WriteLine("Vetor de inteiros");
+Console.WriteLine("  ");
+Console.WriteLine(string.Join(",", integerValues));
+
+Console.WriteLine("********************************");
